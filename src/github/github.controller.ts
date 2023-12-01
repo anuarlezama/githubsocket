2d1c48889b330faf72c86cc7953c8bf42d39b855
alezama <alezama@hushmail.me>
2023-11-30 18:40:36 -0600

    first commit

---
+++ src/github/github.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Headers, Body } from '@nestjs/common';
+import { GithubService } from './github.service';
+
+@Controller('github')
+export class GithubController {
+  constructor(private readonly githubService: GithubService) {}
+
+  @Post( '/' )
+webhookHandler(
+    @Headers( 'x-github-event' ) githubEvent: any,
+    @Body() body: any,
+) {
+
+  console.log({ githubEvent });
+
+  return { githubEvent };
+}
+
+}
+
